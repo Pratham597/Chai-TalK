16d37057216535189f83e07bdc9e2c79121ddb14
Pratham597 <jnvvadodara129@gmail.com>
2024-10-29 23:59:39 +0530

    Changes in backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,11 @@ const server = app.listen(port, () => {
   console.log("App is listening on given port ");
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://chaitalk.vercel.app/"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 const io = new Server(server, {
   cors: {
